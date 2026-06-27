a4f7b1e056813cd6be8e4be74df97f620a4b1ac4
Alex Kahn <alexkahndev@gmail.com>
2026-06-27 12:29:37 -0400

    v0.1.1-beta.0: export the new protocol types (TourTheme/media/advance/etc.) from the barrel

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 export type {
 	TourStep,
 	TourPlacement,
+	TourMediaKind,
+	TourMedia,
+	TourAdvanceOn,
+	TourAdvance,
+	TourSpotlightShape,
+	TourSpotlight,
+	TourTransitionKind,
+	TourTransition,
 	TourTrigger,
+	TourTheme,
 	Tutorial,
 } from "./types";
 export { useTourController, type TourController } from "./controller";
